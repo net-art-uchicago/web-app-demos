53aedc52cc1f03eaba32f7b321bf6aeff880570a
nbriz <nickbriz@gmail.com>
2020-05-21 12:04:25 -0500

    created a custom server for our dogs && forecast app

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/www/index.html`)
+})
+
+app.listen(port, (err) => {
+  if (err) return console.log(err)
+  console.log(`
+    Listening on http://localhost:${port}
+    Document root is ${__dirname}
+    Press Ctrl-C to quit.
+  `)
+})
